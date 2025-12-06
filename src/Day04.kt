ad75981b4215f713478e3a96e0d9389f7ff17de2
FailedCode <xeres666@googlemail.com>
2025-12-06 17:11:37 +0100

    day4: part2

--- src/Day04.kt
+++ src/Day04.kt
@@ -6,12 +6,12 @@ fun main() {
     val input = loadInput("input/day04.txt")
 
     println(">")
-    println(part1(input))
-//    println(part2(lines))
+//    println(part1(input))
+    println(part2(input))
 }
 
-fun loadInput(filepath: String): ArrayList<List<Int>> {
-    val grid: ArrayList<List<Int>> = arrayListOf<List<Int>>()
+fun loadInput(filepath: String): ArrayList<MutableList<Int>> {
+    val grid = arrayListOf<MutableList<Int>>()
     val lines: List<String> = File(filepath).readLines()
     for (line in lines) {
         grid.add(
@@ -19,14 +19,14 @@ fun loadInput(filepath: String): ArrayList<List<Int>> {
             .trim()
             .replace(".", "0")
             .replace("@", "1")
-            .split("")
-            .filterNot { it.isEmpty() }
-            .map { it.toInt() })
+            .map {  it.digitToInt() }
+            .toMutableList()
+        )
     }
     return grid
 }
 
-fun part1(grid: ArrayList<List<Int>>): String {
+fun part1(grid: ArrayList<MutableList<Int>>): String {
     val enableGridDraw = false
     var result: Long = 0
     for (y in grid.indices) {
@@ -47,12 +47,39 @@ fun part1(grid: ArrayList<List<Int>>): String {
     return result.toString()
 }
 
+fun part2(grid: ArrayList<MutableList<Int>>): String {
+    var currentGrid:ArrayList<MutableList<Int>> = ArrayList(grid)
+    var nextGrid:ArrayList<MutableList<Int>> = ArrayList(grid)
+    var result: Long = 0
+    var changesMade: Boolean
+    while (true) {
+        changesMade = false
+        for (y in currentGrid.indices) {
+            for (x in currentGrid[y].indices) {
+                if (currentGrid[y][x] > 0) {
+                    if (countNeighbours(x, y, currentGrid) < 4) {
+                        result += 1
+                        nextGrid[y][x] = 0
+                        changesMade = true
+                    }
+                }
+            }
+        }
+        if (changesMade) {
+            currentGrid = nextGrid
+        } else {
+            break;
+        }
+    }
+    return result.toString()
+}
+
 /**
  * | -1-1 | 0-1 | +1-1 |
  * | -10  | 00  | +10  |
  * | -1+1 | 0+1 | +1+1 |
  */
-fun countNeighbours(x: Int, y: Int, grid: ArrayList<List<Int>>): Int {
+fun countNeighbours(x: Int, y: Int, grid: ArrayList<MutableList<Int>>): Int {
     return (grid.getOrNull(y+1)?.getOrNull(x) ?: 0) +
         (grid.getOrNull(y+1)?.getOrNull(x+1) ?: 0) +
         (grid.getOrNull(y)?.getOrNull(x+1) ?: 0) +
