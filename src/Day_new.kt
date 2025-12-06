63fd46e836818856b070c28909e86af6044b6adf
FailedCode <xeres666@googlemail.com>
2025-12-06 22:02:33 +0100

    added template

---
+++ src/Day_new.kt
@@ -0,0 +1,27 @@
+package aoc.DayX
+
+import java.io.File
+import kotlin.math.*
+
+fun main(){
+    val lines = getInput("input/day0Xexample.txt")
+
+    println(">")
+    println(part1(lines))
+//    println(part2(lines))
+}
+
+fun getInput(filepath: String): List<String> {
+    val lines = File(filepath).readLines()
+    return lines
+}
+
+fun part1(lines:List<String>): String {
+    var result: Long = 0
+    return result.toString()
+}
+
+fun part2(lines:List<String>): String {
+    var result: Long = 0
+    return result.toString()
+}
